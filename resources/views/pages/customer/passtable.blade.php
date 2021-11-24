b19192d9f99166419e466270cbacc97dca3f4604
wnandirta <nandira11@gmail.com>
2021-11-24 20:57:54 +0700

    masukannamaui

--- resources/views/pages/customer/passtable.blade.php
+++ resources/views/pages/customer/passtable.blade.php
@@ -8,16 +8,16 @@
 <body>
     <div class="wrapper">
         <div id="box">
-            <img src="{{asset('assets/images/logo.jpeg')}}" alt="lock" />
+            <img src="{{asset('assets/images/logo.jpeg')}}" alt="lock" /> <br>
             <h3>Meja : {{$no_table}}</h3>
             <h3 class="mb-3">No Pemesanan : {{$no_pesanan}}</h3>
             <form action="{{route('sendNama')}}" method = "POST">
                 @csrf
                 <input type="hidden" name="no_table" value="{{$no_table}}">
-                <input type="hidden" name="no_pesanan" value="{{$no_pesanan}}">
-                <label class = "mb-2"for="" style="color: rgb(61, 116, 235); font-weight: bold;">Masukkan Nama</label>
-                <input type="text" name="nama_pemesan">
-                <button class = "mb-3 mt-2 btn btn-primary" type="Submit">Lanjut Pesan</button>
+                <input type="hidden" name="no_pesanan" value="{{$no_pesanan}}"><br>
+                <label class = "mb-2"for="" style="color: #ffde59; font-weight: bold;">Masukkan Nama</label><br><br>
+                <input type="text" name="nama_pemesan"><br><br>
+                <button class = "mb-3 mt-2 btn btn-warning" type="Submit">Lanjut Pesan</button>
             </form>
         </div>
     </div>
