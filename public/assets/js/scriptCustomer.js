bbfe77a51d9d3c8da17dbec29e399053b3a33dea
MeliskaYI <meliskayaffaivana725myip29@gmail.com>
2021-11-24 15:42:36 +0700

    responsive

--- public/assets/js/scriptCustomer.js
+++ public/assets/js/scriptCustomer.js
@@ -164,8 +164,8 @@ function displayCart() {
 	var output="";
 	for(var i in cartArray) {
 		output+=`<tr>
-			<td>${cartArray[i].name}</td></tr>
-			<td>${cartArray[i].price}</td>
+			<td>${cartArray[i].name}</td>
+			<td>Rp.${cartArray[i].price}</td></tr>
 			<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-id=${cartArray[i].id}>-</button>
 				<input type='number' class='item-count form-control' data-id='${cartArray[i].id}' value='${cartArray[i].count}'>
 				<button class ='plus-item btn btn-primary input-group-addon' data-id="${cartArray[i].id}">+</button></div></td>
--- resources/views/pages/customer/order.blade.php
+++ resources/views/pages/customer/order.blade.php
@@ -83,7 +83,7 @@
 		<!-- Modal -->
 		<div class="modal fade" id="cart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
 			aria-hidden="true">
-			<div class="modal-dialog mx-auto" role="document">
+			<div class="modal-dialog" role="document">
 				<div class="modal-content ">
 					<div class="modal-header">
 						<h5 class="modal-title" id="exampleModalLabel">Cart</h5>
