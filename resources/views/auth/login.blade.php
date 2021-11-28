7b01147a76db6b208f55447d22e1912ea9679d28
widiarsaf <wretasafitri33@gmail.com>
2021-11-29 05:11:20 +0700

    fix responxive page in homepage and loginpage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,97 @@
-@extends('layouts.templateAuth')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="row">
-	<div class="col-sm-12">
-		<!-- Authentication card start -->
-		<div class="login-card card-block auth-body mr-auto ml-auto">
-			<form class="md-float-material" role="form" action="{{ route('login') }}" method="POST">
-				@csrf
-				<div class="text-center">
-					<img src="{{asset('assets')}}/images/logo.jpeg" alt="logo.png" style="height: 100px">
-				</div>
-				<div class="auth-box">
-					<div class="row m-b-20">
-						<div class="col-md-12">
-							<h3 class="text-left txt-primary">Login</h3>
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<title>Baker Bootstrap Template - Index</title>
+	<meta content="" name="description">
+	<meta content="" name="keywords">
+
+	<!-- Favicons -->
+	<link href="{{asset('assets/images/favicon.png')}}" rel="icon">
+	<link href="{{asset('assets/images/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+	<link
+		href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+		rel="stylesheet">
+
+	<!-- Vendor CSS Files -->
+	<link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+	<link href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+	<link href="{{asset('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+	<link href="{{asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+	<!-- Template Main CSS File -->
+	<link href="{{asset('assets/css/styleAuth.css')}}" rel="stylesheet">
+	<style>
+		.containerLogin{
+			padding: 40px;
+			margin: 20px;
+			border-radius: 10px;
+			background-color: rgba(37, 42, 52, 0.7) !important;
+			/* background: #252A34 !important; */
+		}
+		.btn-get-started{
+			background: #ffc107;
+		}
+		.form-control{
+			min-height: 50px !important;
+			border-radius: 5px !important;
+		}
+
+	</style>
+</head>
+
+<body>
+
+
+	<!-- ======= Hero Section ======= -->
+	<section id="hero" class="d-flex align-items-center justify-content-center">
+		<div class="container position-relative">
+			<h1>Welcome Back!</h1>
+			 <center>
+						<div class="col-lg-4 containerLogin" style="background: black">
+							<form role="form" action="{{ route('login') }}" method="POST">
+								@csrf
+								<h2 style="margin-bottom: 20px">Pelase Login First</h2>
+								<div class="input-group">
+									<span class="md-line"></span>
+									<input type="text" class="form-control" name="name" placeholder="Enter your email" >
+									<span class="md-line"></span>
+								</div>
+								<br>
+								<div class="input-group">
+									<span class="md-line"></span>
+									<input type="password" class="form-control" name="password" placeholder="Password">
+									<span class="md-line"></span>
+								</div>
+								<div class="text-center"><button type="submit" class="btn-get-started scrollto">Login</button></div>
+							</form>
 						</div>
-					</div>
-					<hr />
-					<div class="input-group">
-						<input type="text" class="form-control" name="name" placeholder="Enter your name">
-						<span class="md-line"></span>
-					</div>
-					<div class="input-group">
-						<input type="password" class="form-control" name="password" placeholder="Password">
-						<span class="md-line"></span>
-					</div>
-					<div class="row text-right">
-						<div class="col-sm-7 col-xs-12">
-							<a href="auth-reset-password.html" class="text-right f-w-600 text-inverse"> Forgot Your
-								Password?</a>
-						</div>
-					</div>
-					<div class="row m-t-30">
-						<div class="col-md-12">
-							<button type="submit" class="btn btn-warning btn-md btn-block waves-effect text-center m-b-20">Sign
-								in</button>
-						</div>
-					</div>
-					<hr />
-				</div>
-			</form>
-			<!-- end of form -->
-		</div>
-		<!-- Authentication card end -->
-	</div>
-	<!-- end of col-sm-12 -->
-</div>
-@endsection
+						</center>
+			
+	</section><!-- End Hero -->
+
+
+
+	<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+			class="bi bi-arrow-up-short"></i></a>
+
+	<!-- Vendor JS Files -->
+	<script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+	<script src="{{asset('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+	<script src="{{asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+	<script src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
+	<script src="{{asset('assets/vendor/purecounter/purecounter.js')}}"></script>
+	<script src="{{asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+
+	<!-- Template Main JS File -->
+	<script src="{{asset('assets/js/scriptAuth.js')}}"></script>
+
+</body>
+
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,48 +1,107 @@
-@extends('layouts.templateAuth')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<style>
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
+    <title>Baker Bootstrap Template - Index</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-#btnmenu1{
-    
-    display:flex;
-    align-items:center;
-    justify-content: center;
-    transition-duration:0.25s;
-    font-size:large;
-    font-weight: bold;
-    background-color:#ffde59;
-}
+    <!-- Favicons -->
+    <link href="{{asset('assets/images/favicon.png')}}" rel="icon">
+    <link href="{{asset('assets/images/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
-#btnmenu2{
-    
-    display:flex;
-    align-items:center;
-    justify-content: center;
-    transition-duration:0.25s;
-    font-size:large;
-    font-weight: bold;
-    background-color:#545454;
-}
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
-</style>
-<br>
-<div class="text-center">
-					<img src="{{asset('assets')}}/images/logo.jpeg" alt="logo.png" style="height: 100px">
-</div>
-<br>
-<div class="row" style="height:350px;">
-    
-    <div class="col-sm-6" id="btnmenu1" onclick="window.open('https://qrcodescan.in/', '_blank').focus()">
-        <button type="button"  class="btn btn-outline-dark btn-lg ">     Pesan Sekarang     </button>
-    </div>
-    <div class="col-sm-6" id="btnmenu2" onclick="window.open('{{url("login")}}', '_blank').focus()">
-    <button type="button"  class="btn btn-outline-warning btn-lg ">          Log In          </button>
-    </div>
-    <!-- end of col-sm-12 -->
-</div>
-@endsection
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{asset('assets/css/styleAuth.css')}}" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Baker - v4.6.0
+  * Template URL: https://bootstrapmade.com/baker-free-onepage-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top header-transparent">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <h1 class="logo"><a href="index.html">D-MOS</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" onclick="window.open('{{url("login")}}', '_blank').focus()">Login</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center justify-content-center" style = "background: url({{asset('assets/img/hero-bg.jpg')}} top center;">
+        <div class="container position-relative">
+            <h1>Welcome to D-MOS</h1>
+            <h2>We Digitalize a menu order system of restaurant</h2>
+            <a href ="#" class="btn-get-started scrollto" onclick="window.open('https://qrcodescan.in/', '_blank').focus()">Pesan Sekarang</a>
+        </div>
+    </section><!-- End Hero -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+
+        <div class="container d-md-flex py-4">
+
+            <div class="me-md-auto text-center text-md-start">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>D-MOS</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits">
+                    <!-- All the links in the footer should remain intact. -->
+                    <!-- You can delete the links only if you purchased the pro version. -->
+                    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/baker-free-onepage-bootstrap-theme/ -->
+                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>, Customize by D-MOS team
+                </div>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    {{-- <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script> --}}
+
+    <!-- Template Main JS File -->
+    <script src="{{asset('assets/js/scriptAuth.js')}}"></script>
+
+</body>
+
+</html>
 
 
 
